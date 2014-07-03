3044e92f0f1c30770c732c873bd2070191337789
Sean Smith <seansmith1020@gmail.com>
2014-07-02 20:04:38 -0600

    deleted debugging/puts

--- app.rb
+++ app.rb
@@ -13,9 +13,7 @@ class App < Sinatra::Application
   end
 
   post "/signout" do
-    print "SESSION ID BEFORE DELETE #{session[:user_id]}"
     session.delete(:user_id)
-    print "SESSION ID AFTER DELETE #{session[:user_id]}"
     redirect '/'
   end
 
@@ -23,22 +21,15 @@ class App < Sinatra::Application
     user = params[:username]
     pwrd = params[:password]
 
-    print "SESSION ID AFTER REDIRECT #{session[:user_id]}"
-
     @user_database.all.each do |user_hash|
       if user_hash.has_value?(user) && user_hash.has_value?(pwrd)
         session[:user_id] = user_hash[:id]
-        print "this is user hash #{user_hash}"
       end
     end
 
-    print "session ID after iteration"
-
     if session[:user_id]
-      puts "THIS IS THE SESSION INFO #{session[:user_id]}"
       user_info = @user_database.find(session[:user_id])
       user_name = user_info[:username]
-      puts "USER INFO #{user_info}"
       erb :logged_in, :locals => { :name => user_name }
     else
       erb :root, :locals => { :thanks_notice => "" }
